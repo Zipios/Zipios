4751acba4e246494104dd5d2129ebd5d2f9cc97d
Heiko H. <heikoh@users.sf.net>
2007-01-04 17:41:55 +0000

    fixed unit tests to first call getNextEntry() to get entry #1

--- tests/zipinputstreamtest.cpp
+++ tests/zipinputstreamtest.cpp
@@ -7,62 +7,62 @@
 
 #include "zipinputstreamtest.h"
 
-const zipios::TestFiles zipios::ZipInputStreamTest::TEST_FILES;
+namespace zipios {
+
+const TestFiles ZipInputStreamTest::TEST_FILES;
 
 
-void zipios::ZipInputStreamTest::testFirstMethod() {
-
-  std::cout << " Trying to test methods..but I have no clue\n" << std::endl;
- 
+void ZipInputStreamTest::testFirstMethod() {
+  CPPUNIT_FAIL( "Implement this" );
 }
 
-void zipios::ZipInputStreamTest::testZipContents() {
+void ZipInputStreamTest::testZipContents() {
   ZipInputStream zis("test.zip");
-  int count = 1;
+  int count(0);
   while (zis.getNextEntry()->isValid())
     count++;
   CPPUNIT_ASSERT_EQUAL(4, count);
 }
 
-void zipios::ZipInputStreamTest::testZipContentNames() {
+void ZipInputStreamTest::testZipContentNames() {
   vector<string> entries;
   entries.push_back("file1.txt"); // got these from unzip -l test.zip
   entries.push_back("file2.txt");
   entries.push_back("file3.txt");
   entries.push_back("testfile.bin");
   ZipInputStream zis("test.zip");
-  vector<string>::const_iterator it = entries.begin();
-  ConstEntryPointer poi = zis.getNextEntry();
-  int count =1;
+  ConstEntryPointer poi(zis.getNextEntry());
+  int count(0);
   while( poi->isValid() ) {
-      CPPUNIT_ASSERT_EQUAL( entries[count],poi->getName());  
-      poi = zis.getNextEntry();
-      count++;
-  }
-}
-void zipios::ZipInputStreamTest::testZipFileSizes() {
-  vector<int> entries;
-  entries.push_back(1327); // got these from unzip -l test.zip
-  entries.push_back(17992);
-  entries.push_back(8);
-  entries.push_back(76468);
-  ZipInputStream zis("test.zip");
-  ConstEntryPointer poi = zis.getNextEntry();
-  int count = 1;
-  while( poi->isValid() ) {
-      int file_size = poi->getSize();
-      CPPUNIT_ASSERT_EQUAL( entries[count],file_size);     
+      CPPUNIT_ASSERT_EQUAL( entries[count], poi->getName() );
       poi = zis.getNextEntry();
       count++;
   }
 }
 
-void zipios::ZipInputStreamTest::testDirectory() {
+void ZipInputStreamTest::testZipFileSizes() {
+  vector<uint32> entries;
+  entries.push_back(1327); // got these from unzip -l test.zip
+  entries.push_back(17992);
+  entries.push_back(8);
+  entries.push_back(76468);
+  ZipInputStream zis("test.zip");
+  ConstEntryPointer poi(zis.getNextEntry());
+  int count(0);
+  while( poi->isValid() ) {
+      CPPUNIT_ASSERT_EQUAL( entries[count], poi->getSize() );
+      poi = zis.getNextEntry();
+      count++;
+  }
+}
+
+void ZipInputStreamTest::testDirectory() {
   ZipInputStream zis("test.zip"); //only files in this
-  ConstEntryPointer poi = zis.getNextEntry();
-    while( poi->isValid() ) {
-      int isDirectory= poi->isDirectory();
-      CPPUNIT_ASSERT_EQUAL(0,isDirectory);   
+  ConstEntryPointer poi(zis.getNextEntry());
+  while( poi->isValid() ) {
+      CPPUNIT_ASSERT_EQUAL( false, poi->isDirectory() );
       poi = zis.getNextEntry();
   }
+}
+
 }
