cf5ef470044130f33df4085034f64b5e8a402932
AlexisWilke <alexis@m2osw.com>
2015-03-09 16:15:59 -0700

    Added an index to list all coverage available versions.

---
+++ dev/index.php
@@ -0,0 +1,19 @@
+<?php
+# Display this directory to users
+# (code used to display test results)
+
+$dir = glob("zipios*");
+
+echo "<html><head><title>Zipios++ coverage, statistics, and test log information</title></head>";
+echo "<body><h1>Zipios++ coverage</h1><table border=\"1\" cellpadding=\"10\" cellspacing=\"0\"><tbody><tr><th>Coverage</th><th>Statistics</th><th>Test Logs</th></tr>";
+foreach($dir as $d)
+{
+	echo "<tr>";
+
+	echo "<td><a href=\"", $d, "\">", $d, "</a></td>";
+	echo "<td><a href=\"", $d, "/statistics.html\">", $d, "/statistics.html</td>";
+	echo "<td><a href=\"", $d, "/test_log.html\">", $d, "/test_log.html</td>";
+
+        echo "</tr>";
+}
+echo "</tbody></table></body></html>";
