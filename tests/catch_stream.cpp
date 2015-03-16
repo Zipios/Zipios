5cbaa9cf23e187b96b39a9fa7154021b783714c1
AlexisWilke <alexis@m2osw.com>
2015-03-16 13:36:07 -0700

    Added catch_stream.cpp to check low level stream and buffer classes.

---
+++ tests/catch_stream.cpp
@@ -0,0 +1,84 @@
+/*
+  Zipios++ - a small C++ library that provides easy access to .zip files.
+  Copyright (C) 2000-2015  Thomas Sondergaard
+
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2 of the License, or (at your option) any later version.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA
+*/
+
+/** \file
+ *
+ * Zipios++ unit tests for various stream and buffer classes.
+ */
+
+#include "catch_tests.hpp"
+
+#include "zipios++/zipfile.hpp"
+#include "zipios++/zipiosexceptions.hpp"
+#include "src/filterinputstreambuf.hpp"
+#include "src/filteroutputstreambuf.hpp"
+
+#include "src/dostime.h"
+
+#include <fstream>
+
+#include <unistd.h>
+#include <string.h>
+
+
+
+
+
+TEST_CASE("An input filter", "[Buffer]")
+{
+    SECTION("Valid input stream buffer")
+    {
+        zipios_test::auto_unlink_t auto_unlink("input.buf");
+        {
+            std::ofstream out("input.buf", std::ios::out | std::ios::binary);
+            out << "Test file" << std::endl;
+        }
+
+        std::unique_ptr<std::ifstream> in_ptr(new std::ifstream("input.buf", std::ios::in | std::ios::binary));
+        std::unique_ptr<zipios::FilterInputStreambuf> buf_ptr(new zipios::FilterInputStreambuf(in_ptr->rdbuf()));
+    }
+
+    SECTION("Invalid input stream buffer")
+    {
+        REQUIRE_THROWS_AS(new zipios::FilterInputStreambuf(nullptr), zipios::InvalidStateException);
+    }
+}
+
+
+TEST_CASE("An output filter", "[Buffer]")
+{
+    SECTION("Valid output stream buffer")
+    {
+        zipios_test::auto_unlink_t auto_unlink("output.buf");
+
+        std::ofstream out("output.buf", std::ios::out | std::ios::binary);
+        std::unique_ptr<zipios::FilterOutputStreambuf> buf_ptr(new zipios::FilterOutputStreambuf(out.rdbuf()));
+    }
+
+    SECTION("Invalid output stream buffer")
+    {
+        REQUIRE_THROWS_AS(new zipios::FilterOutputStreambuf(nullptr), zipios::InvalidStateException);
+    }
+}
+
+
+
+
+
+// vim: ts=4 sw=4 et
